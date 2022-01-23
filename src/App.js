b22baf31e8c939c642e02990b2ce5ed620c3e5b2
Serhii Pylypenko <jpee.dev@gmail.com>
2022-01-23 18:59:09 +0200

    add config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import propTypes from 'prop-types';
 
-function App() {
+function App({ data }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Version {data}</p>
     </div>
   );
 }
+
+App.propTypes = {
+  data: propTypes.number,
+};
 
 export default App;
