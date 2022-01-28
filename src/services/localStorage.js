e3986abc2aca96a98f1daedcd54e0f9320fac3fa
Serhii Pylypenko <jpee.dev@gmail.com>
2022-01-28 17:11:02 +0200

    currency services(localStorage) add methods to work with local storage

---
+++ src/services/localStorage.js
@@ -0,0 +1,30 @@
+export const isAvailableStorage = () => {
+  try {
+    localStorage.setItem('key', 'value');
+    localStorage.removeItem('key');
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const setDataToLocalStorage = (data = {}) => {
+  isAvailableStorage() && Object.entries(data).forEach(([ key, value ]) => {
+    localStorage.setItem(key.toString(), JSON.stringify(value));
+  });
+};
+
+export const getDataFromLocalStorage = (...keys) => {
+  if (!isAvailableStorage()) {
+    return {};
+  }
+
+  if (keys.length === 1 && keys[0]) {
+    return JSON.parse(localStorage.getItem(keys[0]));
+  }
+
+  return keys.reduce((acc, key) => {
+    acc[key] = JSON.parse(localStorage.getItem(key));
+    return acc;
+  }, {});
+};
