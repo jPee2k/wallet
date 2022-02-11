416cb9522874b9169c5ba678d4f3f84817b7f596
Serhii Pylypenko <jpee.dev@gmail.com>
2022-02-11 16:23:05 +0200

    category-list feat(TransactionTable) add getter for category name by id

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+
+import { useLoader } from '../../hooks/useLoader.js';
+import { useGetTransactionCategoriesQuery } from '../../services/transactionCategoryAPI.js';
+import { addCategories } from '../../app/slices/financeSlice.js';
 
 import Currency from '../../components/Currency';
 import UserCard from '../../components/UserCard';
@@ -12,22 +17,34 @@ import styles from './TransactionTab/styles.module.scss';
 
 const { mainSectionBlock, currencyInfoBlock } = styles;
 
-const HomePage = () => (
-  <React.Fragment>
-    <div className={mainSectionBlock}>
-      <div className={currencyInfoBlock}>
-        <UserCard/>
-        <Currency/>
-      </div>
-      <Outlet/>{/* content from router */}
-    </div>
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const { isLoading, isError, isSuccess, data = [] } = useGetTransactionCategoriesQuery();
 
-    <div style={{ display: 'flex', gap: '30px' }}>
-      <NavButton ariaLabel="transactions page" to="/transactions"><Card/></NavButton>
-      <NavButton ariaLabel="statistics page" to="/statistics"><Chart/></NavButton>
-      <LogOutButton/>
-    </div>
-  </React.Fragment>
-);
+  useLoader({ dispatch, isLoading, isError, isSuccess });
+  useEffect(() => {
+    if (isSuccess && data) {
+      dispatch(addCategories(data));
+    }
+  }, [data]);
+
+  return (
+    <React.Fragment>
+      <div className={mainSectionBlock}>
+        <div className={currencyInfoBlock}>
+          <UserCard/>
+          <Currency/>
+        </div>
+        <Outlet/>{/* content from router */}
+      </div>
+
+      <div style={{ display: 'flex', gap: '30px' }}>
+        <NavButton ariaLabel="transactions page" to="/transactions"><Card/></NavButton>
+        <NavButton ariaLabel="statistics page" to="/statistics"><Chart/></NavButton>
+        <LogOutButton/>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default HomePage;
--- src/pages/Home/TransactionTab/TransactionTable/TableTransaction.jsx
+++ src/pages/Home/TransactionTab/TransactionTable/TableTransaction.jsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 const TRANSACTION_TYPES = { inc: 'INCOME', dec: 'EXPENSE' };
 const { transactionTable, table, decTransaction, newTransaction } = styles;
 
-const TableTransaction = ({ data = [] }) => {
+const TableTransaction = ({ data = [], categories = [] }) => {
   if (data.length === 0) {
     return <p>no transactions</p>;
   }
@@ -31,7 +31,7 @@ const TableTransaction = ({ data = [] }) => {
             <tr key={id} className={getClassName(type, status)}>
               <td>{new Date(transactionDate).toLocaleDateString()}</td>
               <td>{type === TRANSACTION_TYPES.inc ? '+' : '-'}</td>
-              <td>{categoryId}</td>
+              <td>{getCategoryNameByID(categories, categoryId)}</td>
               <td>{comment}</td>
               <td>{amount.toFixed(2)}</td>
               <td>{balanceAfter.toFixed(2)}</td>
@@ -62,12 +62,24 @@ TableTransaction.propTypes = {
       status: PropTypes.string,
     }),
   ).isRequired,
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.oneOf([TRANSACTION_TYPES.inc, TRANSACTION_TYPES.dec]).isRequired,
+    }),
+  ).isRequired,
 };
 
 function getClassName(type, status) {
   const typeClass = (type === TRANSACTION_TYPES.dec) ? decTransaction : null;
   const statusClass = (status === 'new') ? newTransaction : null;
   return `${typeClass} ${statusClass}`.trim();
+}
+
+function getCategoryNameByID(categories, id) {
+  const currItem = categories.find((category) => category.id === id);
+  return currItem?.name || '';
 }
 
 export default TableTransaction;
