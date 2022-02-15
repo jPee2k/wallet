fea8616de2ef432fe6a632691c8faa856557d2c8
Serhii Pylypenko <jpee.dev@gmail.com>
2022-02-15 13:58:04 +0200

    styles fix(ModalLogOut)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
 const Button = ({ children, type = 'button', className = '', disabled = false, onClick: clickHandler }) => (
-  <button className={`${className} ${styles.button}`} type={type} disabled={disabled} onClick={clickHandler}>
+  <button className={`${styles.button} ${className}`.trim()} type={type} disabled={disabled} onClick={clickHandler}>
     {children}
   </button>
 );
