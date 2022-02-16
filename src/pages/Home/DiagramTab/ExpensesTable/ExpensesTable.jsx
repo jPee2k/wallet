66944bbc42d64581e7245e32e20302f88505c21e
rom4uk233 <55912308+rom4uk233@users.noreply.github.com>
2022-02-16 12:38:27 +0200

    DiagramTab fixes

--- src/pages/Home/DiagramTab/ExpensesTable/ExpensesTable.jsx
+++ src/pages/Home/DiagramTab/ExpensesTable/ExpensesTable.jsx
@@ -2,106 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
-const TRANSACTION_TYPES = { inc: 'INCOME', dec: 'EXPENSE' };
-
-const ExpensesTable = ({ data = [], categories = [] }) => {
+const ExpensesTable = ({ data = {} }) => {
   if (data.length === 0) {
     return <p className='noData'> no transactions</p>;
   }
-  const { expensesData, totalIncome, totalExpenses } = ExpensesDataMapper(data, categories);
+  const { expensesData, totalIncome, totalExpenses } = data;
 
-  const { statisticsTable, table } = styles;
+  const {
+    tableContainer, tableContainerHeader, tableContainerHeaderItem,
+    tableContainerList, tableContainerListItem, tableContainerListItemCategory,
+    tableContainerListItemAmount, tableContainerFooter, tableContainerFooterItem,
+  } = styles;
   return (
-    <div className={statisticsTable}>
-      <table className={table}>
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* eslint-disable-next-line max-len */}
-          {expensesData.map(({ id, categoryName, amountData }) => {
-            return (
-              <tr key={id}>
-                <td>{categoryName}</td>
-                <td>{amountData}</td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td>Total income</td>
-            <td>{totalIncome}</td>
-          </tr>
-          <tr>
-            <td>Total expenses</td>
-            <td>{totalExpenses}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className={tableContainer}>
+      <div className={tableContainerHeader}>
+        <div className={tableContainerHeaderItem}>Category</div>
+        <div className={tableContainerHeaderItem}>Amount</div>
+      </div>
+      <div className={tableContainerList}>
+        {expensesData.map(({ categoryName, amountData, color }, index) => (
+          <div key={`diagram-expenses-table-key-${index}`} className={tableContainerListItem} style={{ color }}>
+            <div className={tableContainerListItemCategory}>{categoryName}</div>
+            <div className={tableContainerListItemAmount}>{amountData}</div>
+          </div>
+        ))}
+      </div>
+      <div className={tableContainerFooter}>
+        <div className={tableContainerFooterItem}>
+          Total income: {totalIncome}
+        </div>
+        <div className={tableContainerFooterItem}>
+          Total expenses: {totalExpenses}
+        </div>
+      </div>
     </div>
   );
 };
 
-const ExpensesDataMapper = (data, categories) => {
-  return data.reduce((result, dataItem) => {
-    const { id, categoryId, amount } = dataItem;
-    const amountData = Math.abs(amount);
-
-    if (categoryId === '063f1132-ba5d-42b4-951d-44011ca46262') {
-      // eslint-disable-next-line no-param-reassign
-      result.totalIncome += amountData;
-      return result;
-    }
-
-    const categoryName = getCategoryNameByID(categories, categoryId);
-
-    const existingResultIndex = result.expensesData.findIndex((existingExpensesData) => {
-      return existingExpensesData.categoryName === categoryName;
-    });
-    if (existingResultIndex >= 0) {
-      // eslint-disable-next-line no-param-reassign
-      result.expensesData[existingResultIndex].amountData += amountData;
-      // eslint-disable-next-line no-param-reassign
-      result.totalExpenses = amountData + result.totalExpenses;
-      return result;
-    }
-
-    const expensesData = { id, categoryName, amountData };
-    result.expensesData.push(expensesData);
-    // eslint-disable-next-line no-param-reassign
-    result.totalExpenses = amountData + result.totalExpenses;
-    return result;
-  }, { expensesData: [], totalIncome: 0, totalExpenses: 0 });
-};
-
 ExpensesTable.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      transactionDate: PropTypes.string.isRequired,
-      type: PropTypes.oneOf([TRANSACTION_TYPES.inc, TRANSACTION_TYPES.dec]).isRequired,
-      categoryId: PropTypes.string.isRequired,
-      userId: PropTypes.string.isRequired,
-      comment: PropTypes.string.isRequired,
-      amount: PropTypes.number.isRequired,
-      balanceAfter: PropTypes.number.isRequired,
-      status: PropTypes.string,
-    }),
-  ).isRequired,
-  categories: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      type: PropTypes.oneOf([TRANSACTION_TYPES.inc, TRANSACTION_TYPES.dec]).isRequired,
-    }),
-  ).isRequired,
+  data: PropTypes.shape({
+    expensesData: PropTypes.arrayOf(
+      PropTypes.shape({
+        categoryName: PropTypes.string,
+        amountData: PropTypes.number,
+        color: PropTypes.string,
+      }),
+    ),
+    totalIncome: PropTypes.number,
+    totalExpenses: PropTypes.number,
+  }),
 };
-
-function getCategoryNameByID(categories, id) {
-  const currItem = categories.find((category) => category.id === id);
-  return currItem?.name || '';
-}
 
 export default ExpensesTable;
